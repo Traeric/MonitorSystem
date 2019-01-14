abb5b94a3acc0d10db9bbed00460b90cebd65300
Eric Jin <ericjin163@163.com>
2019-01-14 22:04:03 +0800

    this commit is for batch filt upload, it's file transfer version2git status

--- backend/multitask.py
+++ backend/multitask.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from django import conf
 
 
@@ -27,10 +28,30 @@ class MultiTask(object):
     def cmd(self, host_ids):
         request = self.request
         command = request.POST.get('command', None)
+        self.public(command, host_ids)
+
+    def file_upload(self, host_ids):
+        request = self.request
+        # 获取参数
+        upload_path = request.POST.get("file_path", None)   # 堡垒机文件上传位置
+        remote_path = request.POST.get("remote_path", None)  # 远程主机位置
+        self.public(content="传送本地文件到远程", host_ids=host_ids,
+                    task_type="file_upload", params=[upload_path, remote_path])
+
+    def public(self, content, host_ids, task_type="cmd", params=None):
+        """
+        批量命令共同执行的方法
+        :param content:
+        :param host_ids:
+        :param task_type:
+        :param params:
+        :return:
+        """
         # 创建一条Task记录
         task_obj = self.models.Task.objects.create(
-            content=command,
-            user=request.user
+            task_type=task_type,
+            content=content,
+            user=self.request.user
         )
         # 创建该条记录下的TaskLogDetail记录
         task_log_details = []
@@ -43,10 +64,12 @@ class MultiTask(object):
             task_log_details.append(task_log_obj)
         # 批量创建
         self.models.TaskLogDetail.objects.bulk_create(task_log_details)
-        import subprocess
+        # 脱离Django执行脚本
         path = os.path.join(conf.settings.BASE_DIR, "backend", "task_runner.py")
-        task_script = "python3 {0} {1}".format(path, task_obj.id)
+        if task_type == "cmd":
+            task_script = "python3 {0} {1} {2}".format(path, task_obj.id, task_type)
+        else:
+            task_script = "python3 {0} {1} {2} {3} {4}".format(path, task_obj.id, task_type, params[0], params[1])
         subprocess.Popen(task_script, shell=True)
         self.task_id = task_obj.id
-
 
--- backend/task_runner.py
+++ backend/task_runner.py
@@ -6,38 +6,65 @@ import django
 from concurrent.futures import ProcessPoolExecutor
 
 
-def ssh_connect(task_detail_log):
-    host_to_remote_user = task_detail_log.host_to_remote_users
-    # 模拟一个ssh的客户端
-    ssh = paramiko.SSHClient()
-    # 相当于在ssh连接的时候将秘钥写入known_hosts，相当于ssh连接的时候输入yes
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # 连接
-    try:
-        ssh.connect(
-            hostname=host_to_remote_user.host.ip_addr,
-            port=host_to_remote_user.host.port,
-            username=host_to_remote_user.remote_user.username,
-            password=host_to_remote_user.remote_user.password,
-            timeout=5                       # 配置超时时间为5秒
-        )
-        # 执行命令返回3个结果，标准输入，输出和错误
-        stdin, stdout, stderr = ssh.exec_command(task_detail_log.task.content)
-        # 获取结果
-        stdout_res = stdout.read().decode()
-        stderr_res = stderr.read().decode()
-        # 获取详细的主机命令执行信息
-        task_detail_log.result = stdout_res + stderr_res
-        if stderr_res:
+class TaskRunner(object):
+    """执行批量任务"""
+
+    def cmd(self, task_detail_log):
+        """
+        批量命令的执行
+        :param task_detail_log:
+        :return:
+        """
+        host_to_remote_user = task_detail_log.host_to_remote_users
+        # 模拟一个ssh的客户端
+        ssh = paramiko.SSHClient()
+        # 相当于在ssh连接的时候将秘钥写入known_hosts，相当于ssh连接的时候输入yes
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 连接
+        try:
+            ssh.connect(
+                hostname=host_to_remote_user.host.ip_addr,
+                port=host_to_remote_user.host.port,
+                username=host_to_remote_user.remote_user.username,
+                password=host_to_remote_user.remote_user.password,
+                timeout=5                       # 配置超时时间为5秒
+            )
+            # 执行命令返回3个结果，标准输入，输出和错误
+            stdin, stdout, stderr = ssh.exec_command(task_detail_log.task.content)
+            # 获取结果
+            stdout_res = stdout.read().decode()
+            stderr_res = stderr.read().decode()
+            # 获取详细的主机命令执行信息
+            task_detail_log.result = stdout_res + stderr_res
+            if stderr_res:
+                task_detail_log.status = 2
+            else:
+                task_detail_log.status = 1
+        except Exception as e:
+            task_detail_log.result = e
             task_detail_log.status = 2
-        else:
+        task_detail_log.save()
+        # 关闭连接
+        ssh.close()
+
+    def file_upload(self, task_detail_log):
+        host_to_remote_user = task_detail_log.host_to_remote_users
+        try:
+            transport = paramiko.Transport((host_to_remote_user.host.ip_addr, host_to_remote_user.host.port))
+            transport.connect(username=host_to_remote_user.remote_user.username,
+                              password=host_to_remote_user.remote_user.password)
+
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            # 将sys.argv[3]的文件传送到远程的sys.argv[4]目录
+            local_path = sys.argv[4] if not sys.argv[4].endswith("/") else sys.argv[4].rsplit("/", maxsplit=1)[0]
+            sftp.put(sys.argv[3], "%s/%s" % (local_path, sys.argv[3].rsplit("/", maxsplit=1)[1]))
+            task_detail_log.result = "传送本地文件到远程成功"
             task_detail_log.status = 1
-    except Exception as e:
-        task_detail_log.result = e
-        task_detail_log.status = 2
-    task_detail_log.save()
-    # 关闭连接
-    ssh.close()
+            transport.close()
+        except Exception as e:
+            task_detail_log.result = e
+            task_detail_log.status = 2
+        task_detail_log.save()
 
 
 if __name__ == "__main__":
@@ -54,8 +81,14 @@ if __name__ == "__main__":
     task_id = sys.argv[1]
     task_obj = models.Task.objects.get(id=task_id)
     print("task runner...", task_id)
+    # 获取batch type
+    batch_type = sys.argv[2]
     # 起一个进程池
+    task_runner = TaskRunner()
     pool = ProcessPoolExecutor(10)
     for task_detail_obj in task_obj.tasklogdetail_set.select_related():
-        pool.submit(ssh_connect, task_detail_obj)
+        pool.submit(getattr(task_runner, batch_type), task_detail_obj)
     pool.shutdown(wait=True)
+    # 文件转送完毕，删除在堡垒机上的文件
+    import os
+    os.remove(sys.argv[3])
